f4459699c7a4a586e83b7e8d029ea85f13987e12
sim0809 <87102221+sim0809@users.noreply.github.com>
2026-04-09 07:16:27 +0900

    volume_of_sphere.py added

---
+++ volume_of_sphere.py
@@ -0,0 +1,12 @@
+def calculate_volume_of_sphere(radius)
+	pi = 3.14
+	area = pi * radius * radius
+	return area
+
+radius1 = 50
+area1 = compute_area_of_circle(radius1)
+print(f"The area of circle with radius {radius1} is: {area1}")
+
+radius2 = 80
+area2 = compute_area_of_circle(radius2)
+print(f"The area of circle with radius {radius2} is: {area2}")
